f484893633628d6e7f8333d2c8ddfd3d297490ca
Giffari Zakawaly <giffari.zakawaly@ai.astra.co.id>
2025-05-28 18:51:33 +0800

    feat: adjust project list ui

--- src/components/Content/Experience/Experience.tsx
+++ src/components/Content/Experience/Experience.tsx
@@ -1,6 +1,17 @@
 import { useLanguage } from "@/context/LanguageContext";
 import { TitleSection } from "@/components/TitleSection/TitleSection";
-import { Timeline, Text, Card, Group, Image, Flex, Badge, Accordion, List } from "@mantine/core";
+import {
+    Timeline,
+    Text,
+    Card,
+    Group,
+    Image,
+    Flex,
+    Badge,
+    Accordion,
+    List,
+    ThemeIcon,
+} from "@mantine/core";
 import classes from "./Experience.module.css";
 
 export default function ExperienceScrollWithDots() {
@@ -9,9 +20,19 @@ export default function ExperienceScrollWithDots() {
     return (
         <>
             <TitleSection title1={t.experience.title1} title2={t.experience.title2} />
-            <Timeline active={0} lineWidth={6} my="lg">
+            <Timeline active={-1} lineWidth={6} my="lg">
                 {t.experience.items.map((item, index) => (
-                    <Timeline.Item key={index + 1}>
+                    <Timeline.Item
+                        key={index + 1}
+                        bullet={
+                            <ThemeIcon
+                                size={22}
+                                variant="gradient"
+                                gradient={{ from: "blue", to: "cyan" }}
+                                radius="xl"
+                            />
+                        }
+                    >
                         <Card shadow="md" radius="md" withBorder w={{ md: "600px" }}>
                             <Card.Section p={"md"}>
                                 <Flex justify="space-between" align="center" gap={20}>
@@ -25,12 +46,16 @@ export default function ExperienceScrollWithDots() {
                             </Card.Section>
                             <Card.Section px={"md"}>
                                 <Text className={classes.role}>{item.role}</Text>
-                                <Text c="dimmed" className={classes.period}>{item.period}</Text>
+                                <Text c="dimmed" className={classes.period}>
+                                    {item.period}
+                                </Text>
                             </Card.Section>
                             <Card.Section p={"md"}>
                                 <Group gap="xs">
                                     {item.techStack.map((tech, index) => (
-                                        <Badge key={index + 1} variant="light" color="blue">{tech}</Badge>
+                                        <Badge key={index + 1} variant="light" color="blue">
+                                            {tech}
+                                        </Badge>
                                     ))}
                                 </Group>
                             </Card.Section>
@@ -39,10 +64,12 @@ export default function ExperienceScrollWithDots() {
                                     <Accordion.Item value="detail">
                                         <Accordion.Control>Detail</Accordion.Control>
                                         <Accordion.Panel>
-                                            <List >
+                                            <List>
                                                 {item.responsibilty.map((responsibilty, index) => (
                                                     <List.Item key={index + 1} pb={3} pr={15}>
-                                                        <Text className={classes.responsibilty}>{responsibilty}</Text>
+                                                        <Text className={classes.responsibilty}>
+                                                            {responsibilty}
+                                                        </Text>
                                                     </List.Item>
                                                 ))}
                                             </List>
--- src/components/Content/Projects/Projects.tsx
+++ src/components/Content/Projects/Projects.tsx
@@ -1,7 +1,7 @@
 import { useLanguage } from "@/context/LanguageContext";
 import { TitleSection } from "@/components/TitleSection/TitleSection";
-import { Title, Text, Group } from "@mantine/core";
-import { IconArrowUpRight } from "@tabler/icons-react";
+import { Title, Text, Group, Card } from "@mantine/core";
+import { IconRocket } from "@tabler/icons-react";
 import { useState } from "react";
 import classes from "./Projects.module.css";
 import { useDisclosure } from "@mantine/hooks";
@@ -22,21 +22,30 @@ export const Projects = () => {
         <>
             <TitleSection title1={t.projects.title1} title2={t.projects.title2} />
             {t.projects.items.map((item, index) => (
-                <Group mt="md" justify="space-between" w={"100%"} key={index + 1}>
-                    <IconArrowUpRight
-                        size={25}
-                        stroke={4}
+                <Card
+                    key={index + 1}
+                    mt="md"
+                    shadow="md"
+                    radius="md"
+                    p={0}
+                    withBorder
+                >
+                    <Group
+                        justify="flex-end"
+                        className={classes.item}
                         onClick={() => handleClick(index)}
-                    />
-                    <div style={{ textAlign: "right" }}>
-                        <Title order={1} m={0} tt={"uppercase"}>
-                            {item.name}
-                        </Title>
-                        <Text m={0} c="dimmed" className={classes.associate}>
-                            {item.associate}
-                        </Text>
-                    </div>
-                </Group>
+                    >
+                        <IconRocket size={40} />
+                        <div style={{ textAlign: "right" }}>
+                            <Title order={1} m={0} tt={"uppercase"}>
+                                {item.name}
+                            </Title>
+                            <Text m={0} c="dimmed" className={classes.associate}>
+                                {item.associate}
+                            </Text>
+                        </div>
+                    </Group>
+                </Card>
             ))}
             <Detail
                 opened={opened}
