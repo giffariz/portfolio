bca4018613a92944eee37c6b1578a5950b792eb3
Giffari Zakawaly <giffari.zakawaly@ai.astra.co.id>
2025-06-05 13:00:15 +0800

    fix: error ts text pressure

--- src/components/ReactBits/TextPressure/TextPressure.tsx
+++ src/components/ReactBits/TextPressure/TextPressure.tsx
@@ -221,7 +221,7 @@ const TextPressure: React.FC<TextPressureProps> = ({
                 {chars.map((char, i) => (
                     <span
                         key={i}
-                        ref={(el) => (spansRef.current[i] = el)}
+                        ref={(el) => {spansRef.current[i] = el}}
                         data-char={char}
                         style={{
                             display: 'inline-block',
